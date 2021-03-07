f0a11924c1e3046927cd4449e8939b43b72e4f49
NikitaBsu704 <nik.ryndin@gmail.com>
2021-03-07 12:15:13 +0300

    Start sixth laboratory work.This code was taken from methodical instructions.

---
+++ Lab6/bsu/rfe/java/group7/lab6/Ryndin/var/Start.java
@@ -0,0 +1,16 @@
+package bsu.rfe.java.group7.lab6.Ryndin.var;
+
+import javax.swing.JFrame;
+
+public class Start {
+
+	
+	public static void main(String[] args) 
+	{
+		// —оздать и сделать видимым главное окно приложени€
+		MainFrame frame = new MainFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+}
